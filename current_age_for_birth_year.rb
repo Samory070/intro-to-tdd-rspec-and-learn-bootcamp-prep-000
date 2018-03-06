91467ffee8233a2fe83580fb511a61c9b31b652c
samory harris <samory070@gmail.com>
2018-03-06 13:44:05 +0000

    automatically backed up by learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,5 +1,4 @@
-def current_age_for_birth_year(age)
-  current_year = 2018
-  age_of_person = current_year - age
-  puts age_of_person
+def current_age_for_birth_year(year)
+currrent_year = 2018
+
 end
